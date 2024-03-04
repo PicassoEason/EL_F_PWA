5e3133131f2b6d204af28ce4c350563d0997b53c
picassoeason <easonwu.student@outlook.com>
2024-03-04 20:17:05 +0800

    import tailwind css

---
+++ src/Histroy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function History() {
+  return (
+      <div>
+        <link href="./output.css" rel="stylesheet"></link>
+        <h1 class="text-3xl font-bold underline">
+            Hello History
+        </h1>
+    </div>
+  );
+}
